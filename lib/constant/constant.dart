a7fcd77338805da0876d45fcc17bbe8e9dff89ab
satviksriv <84881053+satviksriv@users.noreply.github.com>
2021-07-15 13:49:25 +0530

    Revert "Revert "authorisation page changes""

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -24,233 +24,273 @@ const SizedBox width5Space = const SizedBox(width: 5.0);
 const SizedBox width20Space = const SizedBox(width: 20.0);
 
 const TextStyle appBarTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 18.0,
   fontWeight: FontWeight.bold,
   color: blackColor,
 );
 
 const TextStyle appBarWhiteTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 18.0,
   fontWeight: FontWeight.bold,
   color: whiteColor,
 );
 
 const TextStyle black12RegularTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 12.0,
   color: blackColor,
 );
 
 const TextStyle black14RegularTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: blackColor,
 );
 
 const TextStyle black16RegularTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 16.0,
   color: blackColor,
 );
 
 const TextStyle black14BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: blackColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle black12MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 12.0,
   color: blackColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle black14MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: blackColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle black16MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 16.0,
   color: blackColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle black18MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 18.0,
   color: blackColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle black14SemiBoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: blackColor,
   fontWeight: FontWeight.w600,
 );
 
 const TextStyle black16SemiBoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 16.0,
   color: blackColor,
   fontWeight: FontWeight.w600,
 );
 
 const TextStyle black18SemiBoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 18.0,
   color: blackColor,
   fontWeight: FontWeight.w600,
 );
 
 const TextStyle black16BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 16.0,
   color: blackColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle black18BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 18.0,
   color: blackColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle black22BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 22.0,
   color: blackColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle white12MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 12.0,
   color: whiteColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle white14MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: whiteColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle white16MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 16.0,
   color: whiteColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle white18MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 18.0,
   color: whiteColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle white48MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 48.0,
   color: whiteColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle white12SemiBoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 12.0,
   color: whiteColor,
   fontWeight: FontWeight.w600,
 );
 
 const TextStyle white16SemiBoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 16.0,
   color: whiteColor,
   fontWeight: FontWeight.w600,
 );
 
 const TextStyle white14BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: whiteColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle white16BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 16.0,
   color: whiteColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle white18BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 18.0,
   color: whiteColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle white26BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 26.0,
   color: whiteColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle white36BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 36.0,
   color: whiteColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle white44BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 44.0,
   color: whiteColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle primaryColor12RegularTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 12.0,
   color: primaryColor,
 );
 
 const TextStyle primaryColor14RegularTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: primaryColor,
 );
 
 const TextStyle primaryColor14MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: primaryColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle primaryColor16MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 16.0,
   color: primaryColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle primaryColor16BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 16.0,
   color: primaryColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle primaryColor18BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 18.0,
   color: primaryColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle primaryColor22BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 22.0,
   color: primaryColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle grey12RegularTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 12.0,
   color: greyColor,
 );
 
 const TextStyle grey14RegularTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: greyColor,
 );
 
 const TextStyle grey12MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 12.0,
   color: greyColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle grey12MediumItalicTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: greyColor,
   fontWeight: FontWeight.w500,
@@ -258,61 +298,85 @@ const TextStyle grey12MediumItalicTextStyle = const TextStyle(
 );
 
 const TextStyle grey14MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: greyColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle grey16MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 16.0,
   color: greyColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle grey12BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 12.0,
   color: greyColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle grey14BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: greyColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle grey16BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 16.0,
   color: greyColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle grey18BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 18.0,
   color: greyColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle grey20BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 20.0,
   color: greyColor,
   fontWeight: FontWeight.bold,
 );
 
 const TextStyle green14MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: greenColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle red14MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 14.0,
   color: redColor,
   fontWeight: FontWeight.w500,
 );
 
 const TextStyle red16MediumTextStyle = const TextStyle(
+  fontFamily: 'Jost',
   fontSize: 16.0,
   color: redColor,
   fontWeight: FontWeight.w500,
+);
+
+const TextStyle red16BoldTextStyle = const TextStyle(
+  fontFamily: 'Jost',
+  fontSize: 16.0,
+  color: redColor,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle black12LightTextStyle = const TextStyle(
+  fontFamily: 'Jost',
+  fontSize: 12.0,
+  color: blackColor,
+  fontWeight: FontWeight.normal,
 );
--- lib/pages/splashScreen.dart
+++ lib/pages/splashScreen.dart
@@ -17,39 +17,71 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Timer(
-        Duration(seconds: 3),
-        () => Navigator.push(
-            context, MaterialPageRoute(builder: (context) => Login())));
+    // Timer(
+    //     Duration(seconds: 500),
+    //     () => Navigator.push(
+    //         context, MaterialPageRoute(builder: (context) => Login())));
   }
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+
+    //Your work starts here****************************************
+
     return Scaffold(
-      backgroundColor: primaryColor,
+      backgroundColor: scaffoldBgColor,
       body: Container(
         width: width,
         height: height,
         child: Stack(
           children: [
-            Container(
-              width: width,
-              height: height,
-              alignment: Alignment.center,
-              child: Text(
-                'CryptoX',
-                style: white44BoldTextStyle,
-              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  width: double.infinity,
+                ),
+                SizedBox(
+                  height: 150,
+                ),
+                Image.asset(
+                  'assets/Logo.png',
+                  width: 150.0,
+                  height: 150.0,
+                  fit: BoxFit.cover,
+                ),
+                Text(
+                  'BKS MyGold',
+                ),
+                SizedBox(
+                  width: 40,
+                  child: Divider(
+                    thickness: 3,
+                  ),
+                ),
+                Text(
+                  'BUY TODAY',
+                ),
+                Text(
+                  'SAVE FOR TOMMOWROW',
+                ),
+                Text(
+                  'A VENTURE OF',
+                ),
+                Text(
+                  'B.K.SARAF PVT JEWELLERS',
+                ),
+              ],
             ),
             Positioned(
               bottom: 30.0,
               child: Container(
                 width: width,
                 child: SpinKitRing(
-                  color: whiteColor,
-                  size: 50.0,
+                  color: primaryColor,
+                  size: 15,
                   lineWidth: 1.5,
                 ),
               ),
@@ -60,3 +92,5 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
+
+//Your work ends here****************************************
